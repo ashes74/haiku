9ae41ba02b5f1f3bd621e837dc08434f46264ef4
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2016-04-16 14:13:59 -0400

    Simple Haiku generator

--- haiku.js
+++ haiku.js
@@ -1,9 +1,6 @@
-// var fs = require('fs');
-// console.log(fs);
-// console.log( fs.readFileSync('./cmudict.txt') );
-
 var fs = require("fs");
 var cmudictFile = readCmudictFile('./cmudict.txt');
+var syllableArr=[];
 
 function readCmudictFile(file){
   return fs.readFileSync(file).toString();
@@ -14,17 +11,49 @@ function formatData(data){
        lineSplit
    lines.forEach(function(line){
     lineSplit = line.split("  ");
-    console.log("The word " + lineSplit[0] + " has this phoneme    layout: " + lineSplit[1]);
-
+    // console.log("The word " + lineSplit[0] + " has this phoneme    layout: " + lineSplit[1]);
+    syllablePlace(lineSplit);
+  //  console.log(lineSplit);
   });
+
+}
+
+function syllablePlace(phoneme){
+  if(phoneme[1]){
+  var count = phoneme[1].match(/(\d)/g);
+  if(count){count=count.length
+    if(!syllableArr[count]){syllableArr[count]=[];}
+    syllableArr[count].push(phoneme[0]);
+  }
+  // console.log(phoneme, count);
+}
 }
 
 formatData(cmudictFile);
 
 function createHaiku(structure){
     console.log("this should log a haiku with the structure " + structure);
+    var phrase;
+    phrase = structure.map(function(lines){
+
+      if(Array.isArray(lines)){
+        //for each element get a word with that syllable count
+        return lines.map(function(syls){
+            return getLines(syls)
+          }).join(' ');
+        }
+      else{
+        return getLines(lines);
+      }
+    }).join('\n');
+    // return phrase;
+    return phrase;
 }
 
+function getLines(syls, arrOfWords){
+  var arrOfWords = syllableArr[syls];
+  return arrOfWords[Math.floor(Math.random() * arrOfWords.length)];
+}
 
 
 module.exports = {
--- haiku_generator.js
+++ haiku_generator.js
@@ -1,2 +1,6 @@
 var haiku = require('./haiku');
-console.log(haiku.createHaiku([5,7,5]));
+console.log(haiku.createHaiku([
+  [2,3],
+  [1,3,3],
+  [3,2]
+]));
